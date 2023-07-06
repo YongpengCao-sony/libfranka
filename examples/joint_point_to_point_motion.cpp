2ac137827543ac88d7cfddc2e6f000ea96dfbf21
YongpenCao <Yongpeng.Cao@sony.com>
2023-07-06 17:46:02 +0900

    Add timer to joint_to_joint

--- examples/joint_point_to_point_motion.cpp
+++ examples/joint_point_to_point_motion.cpp
@@ -48,9 +48,13 @@ int main(int argc, char** argv) {
               << "Press Enter to continue..." << std::endl;
     std::cin.ignore();
     auto start = std::chrono::high_resolution_clock::now();
+    auto stt = std::chrono::duration_cast<std::chrono::seconds>(start);
+
+    std::cout << "Time spent:  " << stt.count() << std::endl;
     robot.control(motion_generator);
     auto end = std::chrono::high_resolution_clock::now();
     auto diff = std::chrono::duration_cast<std::chrono::seconds>(end - start);
+
     std::cout << "Motion finished" << diff.count() << std::endl;
     std::cout << "Motion finished" << diff.count() << std::endl;
     std::cout << "Time spent:  " << diff.count() << std::endl;
