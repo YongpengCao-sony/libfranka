5ccf9d60f1a4e576f9dad8f45410e69e1633a568
YongpenCao <Yongpeng.Cao@sony.com>
2023-07-06 18:15:12 +0900

    Add timer to joint_to_joint

--- examples/joint_point_to_point_motion.cpp
+++ examples/joint_point_to_point_motion.cpp
@@ -53,7 +53,7 @@ int main(int argc, char** argv) {
     auto diff = std::chrono::duration_cast<std::chrono::microseconds>(end - start);
 
     std::cout << "Motion finished" << std::endl;
-    std::cout << "Time spent:  " << diff.count() / 1000000 << std::endl;
+    std::cout << "Time spent:  " << diff.count() << std::endl;
   } catch (const franka::Exception& e) {
     std::cout << e.what() << std::endl;
     return -1;
